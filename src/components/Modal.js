24f335820bf57e3c2fa40e5a1dcd57927f80b7fc
“choanss” <linnea.anne.andersson@gmail.com>
2022-05-30 14:56:32 +0200

    modal function renamed

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,16 +5,16 @@ Modal.setAppElement("#root");
 
 function Modal () {
     const [isOpen, setIsOpen] = useState(false);
-    const toggelModal = () => {
+    const toggleModal = () => {
         setIsOpen(!isOpen);
     };
    
     return (
         <>
         <button className="popup" onClick={toggleModal}>Call Me!</button>
-        <Modal isOpen={isOpen} onRequestClose={toggelModal} contentLabel="Call Me">
+        <Modal isOpen={isOpen} onRequestClose={toggleModal} contentLabel="Call Me">
             <div>Call me at: 2929293</div>
-            <button onClick={toggelModal}>Close modal</button>
+            <button onClick={toggleModal}>Close modal</button>
         </Modal>
         </>
     );
